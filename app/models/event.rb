487d212ea661db3ab0cd4c3b83fc29a0a705d79b
kjg116 <kelcygooch@gmail.com>
2019-08-14 17:23:44 -0500

    Store images for articles and events and return the link to the image

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Event < ActiveRecord::Base
-  has_many :images
+  has_many_attached :images
   belongs_to :school
   acts_as_paranoid
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
 class Image < ActiveRecord::Base
+  validates :file, :name, presence: true
+
   acts_as_paranoid
+
+  def convert_to_file(school_id: nil, name: nil, file: nil)
+    type = file[/[a-z]+;/].chop
+    file = file.gsub(%r{^data:image/[a-z]+;base64,}, "")
+    File.open("storage/#{school_id}_#{name}.#{type}", 'wb') do |f|
+      f.write(Base64.decode64(file))
+    end
+    { file_name: "#{school_id}_#{name}.#{type}", path: "storage/#{school_id}_#{name}.#{type}" }
+  end
+
+  def article
+    Article.find(self.article_id)
+  end
+
+  def event
+    Event.find(self.event_id)
+  end
 end
